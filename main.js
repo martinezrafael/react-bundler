f559aabfc0ede3aca406a4263a7e6b235eeb0af2
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-05-17 23:55:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+(() => {
+  // src/somar.js
+  function somar(a, b) {
+    return a + b;
+  }
+
+  // src/main.js
+  console.log("teste");
+  var resultado = somar(3, 5);
+  console.log(resultado);
+})();
